372eac4aa214a57f40a4fd55ae28a25326dd7050
Nuno Machado <nmiguelmoura@gmail.com>
2016-11-28 23:21:13 +0000

    Correct typo that prevented bleach from install

--- pg_config.sh
+++ pg_config.sh
@@ -2,7 +2,7 @@ apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python sqlalchemy
 apt-get -qqy install python-pip
-pip-install bleach
+pip install bleach
 
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
