09a8dc828b1c2d919862d0d881c93f58106abd46
nmiguelmoura <nmiguelmoura@gmail.com>
2016-11-29 15:35:55 +0000

    Add VIEWS to database schema

--- src/tournament.sql
+++ src/tournament.sql
@@ -1,12 +1,26 @@
-DROP DATABASE IF EXISTS tournament;
-CREATE DATABASE TOURNAMENT;
-\c tournament
-
 CREATE TABLE players( name TEXT,
                       id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE matches ( winner INTEGER FOREIGN KEY REFERENCES players(id),
-                       looser INTEGER FOREIGN KEY REFERENCES players(id),
+CREATE TABLE matches ( winner INTEGER REFERENCES players(id),
+                       loser INTEGER REFERENCES players(id),
                        id SERIAL PRIMARY KEY
 );
+
+CREATE VIEW count_wins AS SELECT players.id, count(matches.winner) as num
+  FROM players LEFT JOIN matches
+  ON players.id=matches.winner
+  GROUP BY players.id
+  ORDER BY num DESC;
+
+CREATE VIEW count_player_matches AS SELECT players.id, count(matches) as num
+  FROM players LEFT JOIN matches
+  ON matches.winner=players.id OR matches.loser=players.id
+  GROUP BY players.id
+  ORDER BY num DESC;
+
+CREATE VIEW count_wins_matches AS SELECT count_wins.id, count_wins.num as wins,
+  count_player_matches.num as matches
+  FROM count_wins JOIN count_player_matches
+  ON count_wins.id=count_player_matches.id
+  ORDER BY count_wins.num DESC;
